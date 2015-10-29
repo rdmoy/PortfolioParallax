f530bcf84a62f2da262ecec7e3610bfdc314fe43
rdmoy <trex2@Ryans-MacBook-Air-3.local>
2015-10-29 07:43:14 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//Scroll
+
+var scrollHeight = document.body.scrollHeight; //height of page
+var winHeight = document.body.clientHeight; //height of window
+
+//Sections
+
+var sections = document.getElementsByClassName("section");
+
+//Set Section Vertical Offset
+// function offset(){
+// 	for (i=0;i<sections.length;i++){
+// 		sections[i].setAttribute("top", (winHeight*i)+"px");
+// 		// sections[i].firstElementChild.style.visibility="hidden";
+// 	}
+// }
+// function changeBG(){
+// 	for sect
+// }
+
+// offset();
