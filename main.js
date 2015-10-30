694f7004032f46bfde41e9ad333a33c4ee7367f5
rdmoy <trex2@Ryans-MacBook-Air-3.local>
2015-10-30 09:04:08 -0500

    good functionality
    got everything to look nice, need to fix fixed height of sections to
    fit overflow of content divs

--- main.js
+++ main.js
@@ -5,17 +5,40 @@ var winHeight = document.body.clientHeight; //height of window
 
 //Sections
 
+var backgrounds = document.getElementsByClassName("background");
 var sections = document.getElementsByClassName("section");
+//Set Background Visibility
 
-//Set Section Vertical Offset
-// function offset(){
-// 	for (i=0;i<sections.length;i++){
-// 		sections[i].setAttribute("top", (winHeight*i)+"px");
-// 		// sections[i].firstElementChild.style.visibility="hidden";
-// 	}
-// }
-// function changeBG(){
-// 	for sect
-// }
 
-// offset();
+function scroll(event){
+	var body = document.querySelector("body");
+	var scrollDist = body.scrollTop; //Distance body scrolled
+	for (i=0;i<sections.length;i++){
+		var topOffset = sections[i].offsetTop
+		var currentPos = topOffset - scrollDist;
+		sections[i].dataset.top = currentPos;
+		bgVisibility(sections[i]);
+		bgScroll(sections[i]);
+	}
+}
+
+function bgScroll(section){
+	var background = section.querySelector("img");
+	var percentage = ((parseInt(section.dataset.top)*2/winHeight) * 25);
+	console.log(percentage);
+	if (section.style.visibility = "visible") background.style.top = percentage + "%";
+}
+
+function bgVisibility(section){
+	var backgroundDiv = section.querySelector("div");
+	if (section.dataset.top<0) backgroundDiv.style.visibility = "visible";
+	if (section.dataset.top>0) backgroundDiv.style.visibility = "hidden";
+
+	}
+	
+document.onscroll = scroll;
+
+
+
+
+
