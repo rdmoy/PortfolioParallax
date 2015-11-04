7856def24db64048feb316cf515b2b2b23538534
rdmoy <trex2@Ryans-MacBook-Air-3.local>
2015-11-03 18:21:36 -0600

    fixed scaling
    got horizontal and vertical scale to work
    img now stays constant after min width and is offset

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var scrollHeight = document.body.scrollHeight; //height of page
 var winHeight = document.body.clientHeight; //height of window
+var body = document.querySelector("body"); //distance body scrolled
 
 //Sections
 
@@ -11,32 +12,65 @@ var sections = document.getElementsByClassName("section");
 
 
 function scroll(event){
-	var body = document.querySelector("body");
 	var scrollDist = body.scrollTop; //Distance body scrolled
 	for (i=0;i<sections.length;i++){
-		var topOffset = sections[i].offsetTop
-		var currentPos = topOffset - scrollDist;
+		var sectionTopOffset = sections[i].offsetTop; //
+		var currentPos = scrollDist - sectionTopOffset;
 		sections[i].dataset.top = currentPos;
+		sections[i].dataset.width = sections[i].clientWidth
 		bgVisibility(sections[i]);
 		bgScroll(sections[i]);
+		bgTranslate(sections[i]);
 	}
+	event.preventDefault();
 }
 
 function bgScroll(section){
-	var background = section.querySelector("img");
-	var percentage = ((parseInt(section.dataset.top)*2/winHeight) * 25);
-	console.log(percentage);
-	if (section.style.visibility = "visible") background.style.top = percentage + "%";
+	var bgImg = section.querySelector("img"); //section background image
+	var sectionTop = section.dataset.top;
+	var bgOffset = -200; //position relative to top of section
+	var percent = ((sectionTop/winHeight)); 
+	bgImg.style.top = (percent * bgOffset)+ "px";
+	
+}
+
+function bgTranslate(section){
+	var bgImg = section.querySelector("img");
+	var paraWindow = section.querySelector(".parallaxWindow");
+	var bgImgHeight = bgImg.dataset.height;
+	bgImg.dataset.height = bgImg.clientHeight //400px target
+
+	bgImg.dataset.width = section.dataset.width;
+
+	var offsetX = bgImg.dataset.offsetX;
+
+	if (paraWindow.clientWidth >= 800){
+		bgImg.dataset.shrink = true;
+		bgImg.style.height = null;
+		bgImg.style.width = 100+"%";
+		bgImg.style.transform = "translate(" + 0 + "px)";
+	
+	}
+	else{
+		bgImg.dataset.shrink = false;
+		bgImg.style.height = 500+"px";
+		bgImg.style.width = null;
+
+		var x = (paraWindow.clientWidth - 800)/2;
+		bgImg.style.transform = "translate(" + x + "px)";
+		offsetX = x;
+	}
+
 }
 
 function bgVisibility(section){
-	var backgroundDiv = section.querySelector("div");
-	if (section.dataset.top<0) backgroundDiv.style.visibility = "visible";
-	if (section.dataset.top>0) backgroundDiv.style.visibility = "hidden";
-
-	}
+	var backgroundDiv = section.querySelector(".background");
+	if (section.dataset.top>0) backgroundDiv.dataset.visibility = "visible";
+	if (section.dataset.top<0) backgroundDiv.dataset.visibility = "hidden";
+}
 	
 document.onscroll = scroll;
+body.onresize = scroll;
 
 
 
